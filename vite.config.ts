1d2e0f98522a94476506bfae4ff739f0a27087b9
Azurac <Azuracek@gmail.com>
2026-05-04 20:23:30 +0200

    Added alias setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/energy_consumption/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  }
 })
